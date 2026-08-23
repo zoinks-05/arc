b2fba0308bbae4ca56f466d84a638b862dbe17e6
ZiyanN <ziyannadeem@outlook.com>
2026-08-23 17:43:35 +1000

    test

--- Arc/ContentView.swift
+++ Arc/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, tld!")
+            Text("Hello, teld!")
         }
         .padding()
     }
